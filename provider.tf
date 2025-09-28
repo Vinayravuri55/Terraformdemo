8c0c3d547598fcdc4944940bd44808d800b11d87
Vinayravuri55 <vinayravuri55@gmail.com>
2025-09-28 10:12:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,10 +14,10 @@ terraform {
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
-  tenant_id                       = "84f1e4ea-8554-43e1-8709-f0b8589ea118"     # Tenant ID
-  subscription_id                 = "80ea84e8-afce-4851-928a-9e2219724c69"     # Subscription ID
-  client_id                       = "cd30bc4b-58eb-4b6c-9931-06ffcc828ae1"     # Service principle Id
-  client_secret                   = "pB.8Q~.eASnG8RhdpAiBA0JeWURuo7fryaW2ec3P" # Service principle secret
+  tenant_id                       = "var.tenant_id"     # Tenant ID
+  subscription_id                 = "var.subscription_id"     # Subscription ID
+  client_id                       = "var.client_id"     # Service principle Id
+  client_secret                   = "var.client_secret" # Service principle secret
 }
 
 # Create a resource group in Azure
