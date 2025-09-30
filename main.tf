ac0353190ec4725d9665c78fc0788dad3cfbfd56
Vinayravuri55 <vinayravuri55@gmail.com>
2025-09-30 21:25:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,4 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
-    site_config {
-      application_stack {
-        current_stack = "dotnet" 
-        dotnet_version = "v8.0" 
-      }
-    }
 }
