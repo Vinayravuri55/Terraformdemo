a4e076b9aebd9e2559dea1547622bbcd5bc96949
Vinayravuri55 <vinayravuri55@gmail.com>
2025-09-30 22:30:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,4 +18,8 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
+    site_config {
+      dotnet_framework_version = "v4.0"
+      scm_type                 = "LocalGit"
+    }
 }
