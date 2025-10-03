d0b109bd60e7068639157fde82c58cd341854523
Vinayravuri55 <vinayravuri55@gmail.com>
2025-10-03 17:11:14 +0530

    staging.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,11 @@ resource "azurerm_app_service" "example" {
       scm_type                 = "LocalGit"
     }
 }
+
+resource "azurerm_app_service_slot" "example" {
+  name                = "${var.company}${var.environment}asstaging"
+  app_service_name    = azurerm_app_service.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
